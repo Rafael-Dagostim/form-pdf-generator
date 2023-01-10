5f33f82e1dbbb827886c8909c8005fc6a579436b
Rafael-Dagostim <75103113+Rafael-Dagostim@users.noreply.github.com>
2023-01-10 12:54:53 -0300

    Added react base structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PDF Form</h1>
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
